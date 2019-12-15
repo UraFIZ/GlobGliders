9fa9af0f39f6a1a36c92f7f63ca2cc181024e696
UraFIZ <kratser88@gmail.com>
2019-12-15 02:16:11 +0200

    site-banner created

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".navbar-toggler").on('click', function(){
+        console.log($(".navbar-collapse").hasClass("show"))
+       if(!$(".navbar-collapse").hasClass("show")) {
+            $("#navbarSupportedContent").addClass("custom-extend-navbar");
+            $(".js-sity-margin").css("margin-top", "400px");
+        }else{
+            $(".js-sity-margin").css("margin-top", "0");
+        }
+    })
+      
+})
