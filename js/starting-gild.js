0273736903bb44d890b9af1753136b228d276753
Iurii <Iurii@VALENTIN>
2020-01-15 20:00:49 +0200

    staring glid section is done not responsive

---
+++ js/starting-gild.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    onClickPopularLinks()
+})
+
+function onClickPopularLinks() {
+    var searchInput = $("#location");
+    var setOfPopularLocation = $(".popular-location-item");
+    $(setOfPopularLocation).each(function(inx, item) {
+        $(item).click(function(){
+            $(searchInput).val($(this).text());
+        })
+    })
+}
