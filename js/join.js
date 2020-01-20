77f11b7a2f22b610e93805472e08bd8f55c29687
Iurii <Iurii@VALENTIN>
2020-01-20 19:32:06 +0200

    some changes

--- js/join.js
+++ js/join.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $("#join-phone").intlTelInput({
-        allowDropdown:true,
-        autoPlaceholder:"polite",
-        separateDialCode:true,
-    })
+    if($(".join-step-two").length > 0) {
+        $("#join-phone").intlTelInput({
+            allowDropdown:true,
+            autoPlaceholder:"polite",
+            separateDialCode:true,
+        })
+    }
+
 })
