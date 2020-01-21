14fc5a070bd399607387858676738984dafe8d9a
Iurii <Iurii@VALENTIN>
2020-01-21 14:08:02 +0200

    sweetch to the server links

--- js/join.js
+++ js/join.js
@@ -23,16 +23,16 @@ function getToLocalStorageData1(cls) {
 if(!!name && !!lastName) {
     localStorage.setItem("name", name);
     localStorage.setItem("last-name", lastName);
-    window.location.href = "http://127.0.0.1:5502/join-step-two.html";
+    // window.location.href = "http://127.0.0.1:5502/join-step-two.html";
 }
-    // window.location.href = "https://infallible-boyd-0f87d8.netlify.com/join-step-two.html"
+    window.location.href = "https://infallible-boyd-0f87d8.netlify.com/join-step-two.html"
  })
 }
 function getToLocalStorageData5(cls) {
     $(cls).click(function(e){
        e.preventDefault();
        localStorage.setItem("finish", true)
-       window.location.href = "http://127.0.0.1:5502/starting-gild.html"
-       // window.location.href = "https://infallible-boyd-0f87d8.netlify.com/starting-gild.html"
+    //    window.location.href = "http://127.0.0.1:5502/starting-gild.html"
+       window.location.href = "https://infallible-boyd-0f87d8.netlify.com/starting-gild.html"
     })
    }
