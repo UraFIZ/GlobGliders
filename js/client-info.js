b93d3ce5f387e277dc9a49366a177ade205e4744
UraFIZ <kratser88@gmail.com>
2020-01-27 01:27:24 +0200

    add auto changeing item

--- js/client-info.js
+++ js/client-info.js
@@ -10,28 +10,52 @@ $(document).ready(function(){
     })
     getScrollingToTheItem();
     navbarFixed(removeWhiteTheme, addWhiteTheme);
+    getChangeMenuItemsWhileScrolling();
+
 })
 
 function getScrollingToTheItem() {
     $(".special-info-menu-wrapper").on('click',".menu-item", function() {
+        var itemPosition = 0;
         var btnsIndex = $(this).index();
         var item = $(this).parents(".left-info-part").siblings().find(".spacial-info-article-wrapper").eq(btnsIndex);
-        var itemPosition = 0
+        itemPosition = findMiddleHeightItemForScrolling(item);
+        $(this).addClass('menu-item-active').siblings().removeClass('menu-item-active');
+        $("html, body").animate({ scrollTop:  itemPosition}, 1000);
+      });
+}
+function findMiddleHeightItemForScrolling(item) {
         var itemOffset = $(item).offset().top;
         var itemHeight = $(item).height();
         var windowHeight = $(window).height();
         if (itemHeight < windowHeight) {
-            itemPosition = itemOffset - ((windowHeight / 2) - (itemHeight / 2));
+            return itemOffset - ((windowHeight / 2) - (itemHeight / 2));
         } else {
-            itemPosition = itemOffset;
+            return itemPosition = itemOffset;
         };
-        $(this).addClass('menu-item-active').siblings().removeClass('menu-item-active');
-        $("html, body").animate({ scrollTop:  itemPosition}, 1000);
-      });
 }
 function removeWhiteTheme() {
     $("header").removeClass("special-info-header");
 }
 function addWhiteTheme() {
     $("header").addClass("special-info-header");
+}
+function getChangeMenuItemsWhileScrolling() {
+    var scrollingItems = $(".spacial-info-article-wrapper");
+    var scrollItemsHeightArr = [];
+    $(scrollingItems).each(function (inx, item) {
+        scrollItemsHeightArr[inx] = findMiddleHeightItemForScrolling(item);
+        
+    })
+    var menuItems = $(".menu-item");
+    $(window).scroll(function () {
+        var scrollItem = $(this).scrollTop();
+        $(scrollItemsHeightArr).each(function (inx, item) {
+            if(item <= scrollItem) {
+                $(menuItems).removeClass("menu-item-active");
+                $(menuItems).eq(inx).addClass("menu-item-active");
+            }
+          })
+
+    }).scroll();
 }
